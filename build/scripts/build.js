6ebfcdc6db7f0dba984c6ef543282715515ea448
sakthie <vel.cs1986@gmail.com>
2017-07-22 11:45:58 -0700

    Add files via upload

--- build/scripts/build.js
+++ build/scripts/build.js
@@ -11,15 +11,16 @@ singtelApp.config(['growlProvider', function (growlProvider) {
 
 singtelApp.controller('ProgressController', function ($scope, restService, $timeout, growl) {
     /*RestAPI Call*/
-    var progressBarList = restService.getProgressList(success, error);
-
+    $scope.progressBarList = restService.getProgressList(success, error);
+    $scope.greeting = "Welcome!";
     /*RestAPI Success Method*/
     function success(response) {
         if (response.data != null) {
+            $scope.response = response;
             $scope.limit = parseFloat(response.data.limit);
             $scope.bars = response.data.bars;
             $scope.buttons = response.data.buttons;
-
+            $scope.valid = true;
             $scope.finalbars = [];
             angular.forEach($scope.bars, function (value, key) {
                 $scope.finalbars.push({
@@ -31,9 +32,10 @@ singtelApp.controller('ProgressController', function ($scope, restService, $time
         }
     }
 
-      /*RestAPI Error Method*/
+    /*RestAPI Error Method*/
     function error(response) {
         console.log(response);
+        $scope.valid = false;
     }
 
     /*Button Click*/
@@ -61,6 +63,51 @@ singtelApp.controller('ProgressController', function ($scope, restService, $time
         }
     }
 });
+
+describe("init", function () {
+    beforeEach(module('singtelApp'));
+    var ProgressController,
+        scope;
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        HelloWorldController = $controller('ProgressController', {
+            $scope: scope
+        });
+    }));
+    it('says Welcome!', function () {
+        expect(scope.greeting).toEqual("Welcome!");
+    });
+
+});
+
+
+describe("Services", function () {
+    beforeEach(module("singtelApp"));
+    describe("ProgressBar Services", function () {
+        var service, $httpBackend;
+        beforeEach(inject(function ($injector21) {
+            service = $injector.get('restService');
+            it('not null', function () {
+                expect(service).not.toBe(null);
+            });
+
+            it('success', function () {
+                var callback = function () {
+                    var json = parse(scope.response);
+                    success(json);
+                }
+            });
+        }));
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+    });
+});
+
+
+
+
 (function (ng) {
     var utils = ng.module("restServiceModule", []);
 
